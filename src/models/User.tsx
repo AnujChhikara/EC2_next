ec4776b5808b467136b3d7c3a59f1f73c1f264a1
AnujChhikara <anujchhikara777@gmail.com>
2024-12-19 11:43:10 +0530

    added webhook

--- src/models/User.tsx
+++ src/models/User.tsx
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  clerkUserId: { type: String, required: true, unique: true }, // Unique identifier from Clerk
+  clerkId: { type: String, required: true, unique: true }, // Unique identifier from Clerk
   email: { type: String, required: true }, // Email from Clerk
   name: { type: String }, // User's name
   avatar: { type: String }, // URL to the user's profile picture
-  createdAt: { type: Date, default: Date.now }, // Timestamp for user creation
-  additionalData: { type: Object }, // Custom fields for your application
+  firstName: { type: String }, // User's first name
+  lastName: { type: String }, // User's last name
+  username: { type: String }, // User's username
 });
 
 const UserModel = mongoose.models.User || mongoose.model("User", UserSchema);
