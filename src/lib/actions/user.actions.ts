f7e525d46e2a58c04d84242bc06380a0dbc0cbb2
AnujChhikara <anujchhikara777@gmail.com>
2024-12-19 11:10:50 +0530

    build fail fix-1

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 import { UserModel } from "@/models/User";
 import dbConnect from "../dbConnect";
 
-export async function createUser(user) {
+export async function createUser(user: any) {
   try {
     await dbConnect();
     const newUser = await UserModel.create(user);
